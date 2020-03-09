41577d29a0f2c70412b9341195f9daa8aa7f56c5
young-zy <youngzhaozhou@gmail.com>
2020-03-09 10:43:49 +0800

    add Gson dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,6 +41,7 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("com.google.code.gson:gson:2.8.6")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     runtimeOnly("mysql:mysql-connector-java")
     kapt("org.springframework.boot:spring-boot-configuration-processor")

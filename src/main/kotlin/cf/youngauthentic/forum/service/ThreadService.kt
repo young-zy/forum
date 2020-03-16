e47b2d5c739dc2e911e4922329b6039bb6419e3a
young-zy <youngzhaozhou@gmail.com>
2020-03-17 00:10:44 +0800

    add doc notation to threadService

--- src/main/kotlin/cf/youngauthentic/forum/service/ThreadService.kt
+++ src/main/kotlin/cf/youngauthentic/forum/service/ThreadService.kt
@@ -29,6 +29,16 @@ class ThreadService {
     @Autowired
     lateinit var voteRepo: VoteRepository
 
+    /**
+     * get the thread of the given threadId
+     *
+     * @param token token of requested user
+     * @param threadId the threadId of request
+     * @param page page of replies, default value is 1
+     * @param size size of each page, default value is 10
+     * @throws NotFoundException when thread not found
+     * @throws AuthException when user's auth is not enough
+     */
     fun getThread(token: String, threadId: Int, page: Int = 1, size: Int = 10): ThreadObject {
         val tokenObj = loginService.getToken(token)
         val threadProjection = threadRepo.findByTid(threadId)
@@ -50,8 +60,18 @@ class ThreadService {
         return thread
     }
 
+    /**
+     * post a new thread
+     * @param token token of the operator
+     * @param sectionId section of the thread to be posted
+     * @param title thread title
+     * @param content content of the thread
+     * @param isQuestion tell whether this thread is a question
+     * @throws AuthException when operator's auth is not enough
+     * @throws NotFoundException when section not found
+     */
     @Transactional
-    @Throws(AuthException::class)
+    @Throws(AuthException::class, NotFoundException::class)
     fun postThread(token: String?, sectionId: Int, title: String, content: String, isQuestion: Boolean) {
         val tokenObj = loginService.getToken(token)
         //TODO hasAuth
@@ -61,12 +81,20 @@ class ThreadService {
         replyRepo.save(replyEntity)
     }
 
+    /**
+     * post a new reply to the thread
+     * @param token token of the operator
+     * @param threadId section of the thread to be posted
+     * @param replyContent thread title
+     * @throws AuthException when operator's auth is not enough
+     * @throws NotFoundException when section not found
+     */
     @Transactional
     @Throws(AuthException::class, NotFoundException::class)
     fun postReply(token: String, threadId: Int, replyContent: String) {
         val tokenObj = loginService.getToken(token)
         if (!threadRepo.existsById(threadId)) {
-            throw NotFoundException()
+            throw NotFoundException("thread not found")
         }
         // TODO hasAuth
         val replyEntity = ReplyEntity(tid = threadId, replyContent = replyContent, uid = tokenObj!!.uid)

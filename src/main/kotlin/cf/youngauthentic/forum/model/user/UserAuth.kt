695ddccca11d387741591d9988f6658111db4de8
young-zy <youngzhaozhou@gmail.com>
2020-03-17 00:08:35 +0800

    change sections list in UserAuth.kt from List to MutableList

--- src/main/kotlin/cf/youngauthentic/forum/model/user/UserAuth.kt
+++ src/main/kotlin/cf/youngauthentic/forum/model/user/UserAuth.kt
@@ -3,7 +3,7 @@ package cf.youngauthentic.forum.model.user
 data class UserAuth(
         var isSystemAdmin: Boolean = false,
         var isSectionAdmin: Boolean = false,
-        var sections: List<Int> = mutableListOf(),
+        var sections: MutableList<Int> = mutableListOf(),
         var isUser: Boolean = false,
         var isBlocked: Boolean = false
 )

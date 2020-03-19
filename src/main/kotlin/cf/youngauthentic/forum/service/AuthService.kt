2bfc69531c5a7639cd4e6d3faedc5068d334cb72
young-zy <youngzhaozhou@gmail.com>
2020-03-19 23:00:07 +0800

    create auth service

--- src/main/kotlin/cf/youngauthentic/forum/service/AuthService.kt
+++ src/main/kotlin/cf/youngauthentic/forum/service/AuthService.kt
@@ -1,5 +1,7 @@
 package cf.youngauthentic.forum.service
 
+import cf.youngauthentic.forum.model.Token
+import cf.youngauthentic.forum.service.exception.AuthException
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
@@ -8,8 +10,59 @@ class AuthService {
     @Autowired
     lateinit var loginService: LoginService
 
-    fun hasAuth(token: String, config: AuthConfig) {
-        val auth = loginService.getAuth(token)
-
+    /**
+     * check whether user has permission to the correspond operation
+     * @author young-zy
+     * @param tokenObj token object of user
+     * @param config AuthConfig of the operation
+     * @throws AuthException when auth level is not enough
+     */
+    @Throws(AuthException::class)
+    fun hasAuth(tokenObj: Token?, config: AuthConfig) {
+        if (!config.allowAuthor && !config.allowOnlyAuthor && config.authLevel == AuthLevel.UN_LOGGED_IN) {
+            return      // when un logged in user(without token) is allowed to do the operation
+        }
+        if (tokenObj === null) {
+            throw AuthException("Not logged in")    //when operation needs log in
+        }
+        if (config.allowAuthor) {
+            if (config.authorUid != tokenObj.uid) {       //when user is not author
+                if (config.allowOnlyAuthor) {
+                    throw AuthException("user ${tokenObj.uid} is not allowed to do the operation")
+                }
+            } else {                                      //when user is author
+                return
+            }
+        }
+        when (config.authLevel) {
+            AuthLevel.UN_LOGGED_IN -> {
+                return      // blocked user can also operate un logged in operations
+            }
+            AuthLevel.USER -> {
+                if (tokenObj.auth.isBlocked) {
+                    throw AuthException("user ${tokenObj.uid} is blocked, please contact admin for more info")
+                }
+            }
+            AuthLevel.SECTION_ADMIN -> {
+                if (tokenObj.auth.isBlocked) {
+                    throw AuthException("user ${tokenObj.uid} is blocked, please contact admin for more info")
+                }
+                if (tokenObj.auth.isSystemAdmin) {
+                    return
+                } else if (tokenObj.auth.isSectionAdmin && (config.sectionId in tokenObj.auth.sections)) {
+                    return
+                } else {
+                    throw AuthException("user ${tokenObj.uid} is not allowed to do the operation, minimum auth is section admin of ${config.sectionId}")
+                }
+            }
+            AuthLevel.SYSTEM_ADMIN -> {
+                if (tokenObj.auth.isBlocked) {
+                    throw AuthException("user ${tokenObj.uid} is blocked, please contact admin for more info")
+                }
+                if (!tokenObj.auth.isSystemAdmin) {
+                    throw AuthException("user ${tokenObj.uid} is not allowed to do the operation, minimum auth is system admin")
+                }
+            }
+        }
     }
 }

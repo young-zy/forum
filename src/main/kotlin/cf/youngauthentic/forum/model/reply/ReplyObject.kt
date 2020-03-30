a0f5e238fff487462bd8afb1296078e29bfe03d1
young-zy <youngzhaozhou@gmail.com>
2020-03-31 00:06:24 +0800

    change vote info to mutable

--- src/main/kotlin/cf/youngauthentic/forum/model/reply/ReplyObject.kt
+++ src/main/kotlin/cf/youngauthentic/forum/model/reply/ReplyObject.kt
@@ -8,5 +8,5 @@ class ReplyObject(replyProjection: ReplyProjection) {
     val priority = replyProjection.priority
     val isBestAnswer = replyProjection.bestAnswer
     val user = replyProjection.userByUid
-    val vote = 0
+    var vote = 0
 }

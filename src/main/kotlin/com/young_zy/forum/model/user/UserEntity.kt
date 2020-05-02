c4e42cea774f3007a753056671816e8bd0fd08f5
young-zy <youngzhaozhou@gmail.com>
2020-05-03 01:40:27 +0800

    let user entity implement simpleUser projection interface

--- src/main/kotlin/com/young_zy/forum/model/user/UserEntity.kt
+++ src/main/kotlin/com/young_zy/forum/model/user/UserEntity.kt
@@ -10,10 +10,10 @@ data class UserEntity(
         @Column(name = "uid", nullable = false)
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
-        var uid: Int = 0,
+        override var uid: Int = 0,
         @Column(name = "username", nullable = false, length = 45)
         @Basic
-        var username: String = "",
+        override var username: String = "",
         @Column(name = "email", nullable = false, length = 45)
         @Basic
         var email: String = "",
@@ -29,7 +29,7 @@ data class UserEntity(
         @Column(name = "tag_priority", nullable = false)
         @Basic
         var tagPriority: String = "0"
-)
+) : SimpleUser
 
 @Converter
 class UserAuthConverter : AttributeConverter<UserAuth, String> {

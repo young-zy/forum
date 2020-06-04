75116d8d89a320ac6006aab5a81e4cf57dc17a96
young-zy <youngzhaozhou@gmail.com>
2020-06-04 17:59:43 +0800

    change Date to LocalDate

--- src/main/kotlin/com/young_zy/forum/model/user/DetailedUser.kt
+++ src/main/kotlin/com/young_zy/forum/model/user/DetailedUser.kt
@@ -1,11 +1,11 @@
 package com.young_zy.forum.model.user
 
-import java.util.*
+import java.time.LocalDate
 
 data class DetailedUser(
         val uid: Long,
         val username: String,
         val email: String,
         val auth: UserAuth,
-        val regDate: Date
+        val regDate: LocalDate
 )

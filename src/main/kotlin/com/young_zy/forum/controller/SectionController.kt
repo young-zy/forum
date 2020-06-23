10390f8f7aa28c4cf5a77dc824bfd92fc14e8273
young-zy <youngzhaozhou@gmail.com>
2020-06-23 22:48:25 +0800

    refactor code to use exception handler

--- src/main/kotlin/com/young_zy/forum/controller/SectionController.kt
+++ src/main/kotlin/com/young_zy/forum/controller/SectionController.kt
@@ -1,17 +1,11 @@
 package com.young_zy.forum.controller
 
-import com.young_zy.forum.config.stackTraceString
 import com.young_zy.forum.controller.request.AddSectionRequest
 import com.young_zy.forum.controller.response.Response
 import com.young_zy.forum.controller.response.SectionListResponse
 import com.young_zy.forum.controller.response.SectionResponse
 import com.young_zy.forum.service.RateLimitService
 import com.young_zy.forum.service.SectionService
-import com.young_zy.forum.service.exception.AuthException
-import com.young_zy.forum.service.exception.NotFoundException
-import com.young_zy.forum.service.exception.RateLimitExceededException
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
@@ -28,130 +22,61 @@ class SectionController {
     @Autowired
     private lateinit var rateLimitService: RateLimitService
 
-    val logger: Logger = LoggerFactory.getLogger(this.javaClass)
-
     @GetMapping("/section/{sectionId}")
-    suspend fun getSection(@PathVariable sectionId: Int,
+    suspend fun getSection(@PathVariable sectionId: Long,
                            @RequestHeader headers: Map<String, String>,
                            @RequestParam page: Int?,
                            @RequestParam size: Int?): ResponseEntity<Response> {
-        var responseBody: Response? = null
-        var status = HttpStatus.OK
+        val status = HttpStatus.OK
         val responseHeaders = HttpHeaders()
-        try {
-            rateLimitService.buildHeader(headers, responseHeaders)
-            responseBody = SectionResponse(
-                    sectionService.getSection(
-                            headers["token"] ?: "",
-                            sectionId, page ?: 1, size ?: 10
-                    )
-            )
-        } catch (e: RateLimitExceededException) {
-            status = HttpStatus.TOO_MANY_REQUESTS
-            responseBody = Response(false, e.message)
-        } catch (e: NotFoundException) {
-            status = HttpStatus.NOT_FOUND
-            responseBody = Response(false, e.message)
-        } catch (e: AuthException) {
-            status = HttpStatus.UNAUTHORIZED
-            responseBody = Response(false, e.message)
-        } catch (e: Exception) {
-            status = HttpStatus.INTERNAL_SERVER_ERROR
-            logger.error(e.stackTraceString)
-            responseBody = Response(false, e.message ?: "")
-        } finally {
-            return ResponseEntity
-                    .status(status)
-                    .headers(responseHeaders)
-                    .body(responseBody)
-        }
+        rateLimitService.buildHeader(headers, responseHeaders)
+        val responseBody = SectionResponse(
+                sectionService.getSection(
+                        headers["token"] ?: "",
+                        sectionId, page ?: 1, size ?: 10
+                )
+        )
+        return ResponseEntity
+                .status(status)
+                .headers(responseHeaders)
+                .body(responseBody)
     }
 
     @GetMapping("/section")
     suspend fun getSections(@RequestHeader headers: Map<String, String>): ResponseEntity<Response> {
-        var responseBody: Response? = null
-        var status = HttpStatus.OK
+        val status = HttpStatus.OK
         val responseHeaders = HttpHeaders()
-        try {
-            rateLimitService.buildHeader(headers, responseHeaders)
-            responseBody = SectionListResponse(sectionService.getSectionList(headers["token"] ?: ""))
-        } catch (e: RateLimitExceededException) {
-            status = HttpStatus.TOO_MANY_REQUESTS
-            responseBody = Response(false, e.message)
-        } catch (e: NotFoundException) {
-            status = HttpStatus.NOT_FOUND
-            responseBody = Response(false, e.message)
-        } catch (e: AuthException) {
-            status = HttpStatus.UNAUTHORIZED
-            responseBody = Response(false, e.message)
-        } catch (e: Exception) {
-            status = HttpStatus.INTERNAL_SERVER_ERROR
-            logger.error(e.stackTraceString)
-            responseBody = Response(false, e.message ?: "")
-        } finally {
-            return ResponseEntity
-                    .status(status)
-                    .headers(responseHeaders)
-                    .body(responseBody)
-        }
+        rateLimitService.buildHeader(headers, responseHeaders)
+        val responseBody = SectionListResponse(sectionService.getSectionList(headers["token"] ?: ""))
+        return ResponseEntity
+                .status(status)
+                .headers(responseHeaders)
+                .body(responseBody)
     }
 
     @PostMapping("/section")
     suspend fun addSection(@RequestHeader headers: Map<String, String>, @RequestBody requestBody: AddSectionRequest): ResponseEntity<Response> {
-        var responseBody: Response? = null
-        var status = HttpStatus.OK
+        val responseBody: Response? = null
+        val status = HttpStatus.OK
         val responseHeaders = HttpHeaders()
-        try {
-            rateLimitService.buildHeader(headers, responseHeaders)
-            sectionService.addSection(headers["token"] ?: "", requestBody.sectionName)
-        } catch (e: RateLimitExceededException) {
-            status = HttpStatus.TOO_MANY_REQUESTS
-            responseBody = Response(false, e.message)
-        } catch (e: NotFoundException) {
-            status = HttpStatus.NOT_FOUND
-            responseBody = Response(false, e.message)
-        } catch (e: AuthException) {
-            status = HttpStatus.UNAUTHORIZED
-            responseBody = Response(false, e.message)
-        } catch (e: Exception) {
-            status = HttpStatus.INTERNAL_SERVER_ERROR
-            logger.error(e.stackTraceString)
-            responseBody = Response(false, e.message ?: "")
-        } finally {
-            return ResponseEntity
-                    .status(status)
-                    .headers(responseHeaders)
-                    .body(responseBody)
-        }
+        rateLimitService.buildHeader(headers, responseHeaders)
+        sectionService.addSection(headers["token"] ?: "", requestBody.sectionName)
+        return ResponseEntity
+                .status(status)
+                .headers(responseHeaders)
+                .body(responseBody)
     }
 
     @DeleteMapping("/section/{sectionId}")
-    suspend fun deleteSection(@RequestHeader headers: Map<String, String>, @PathVariable sectionId: Int): ResponseEntity<Response> {
-        var responseBody: Response? = null
-        var status = HttpStatus.OK
+    suspend fun deleteSection(@RequestHeader headers: Map<String, String>, @PathVariable sectionId: Long): ResponseEntity<Response> {
+        val status = HttpStatus.OK
         val responseHeaders = HttpHeaders()
-        try {
-            rateLimitService.buildHeader(headers, responseHeaders)
-            sectionService.deleteSection(headers["token"] ?: "", sectionId)
-            responseBody = Response()
-        } catch (e: RateLimitExceededException) {
-            status = HttpStatus.TOO_MANY_REQUESTS
-            responseBody = Response(false, e.message)
-        } catch (e: NotFoundException) {
-            status = HttpStatus.NOT_FOUND
-            responseBody = Response(false, e.message)
-        } catch (e: AuthException) {
-            status = HttpStatus.UNAUTHORIZED
-            responseBody = Response(false, e.message)
-        } catch (e: Exception) {
-            status = HttpStatus.INTERNAL_SERVER_ERROR
-            logger.error(e.stackTraceString)
-            responseBody = Response(false, e.message ?: "")
-        } finally {
-            return ResponseEntity
-                    .status(status)
-                    .headers(responseHeaders)
-                    .body(responseBody)
-        }
+        rateLimitService.buildHeader(headers, responseHeaders)
+        sectionService.deleteSection(headers["token"] ?: "", sectionId)
+        val responseBody = Response()
+        return ResponseEntity
+                .status(status)
+                .headers(responseHeaders)
+                .body(responseBody)
     }
 }

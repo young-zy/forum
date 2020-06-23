0e504e6d2f7d1cdb6f86211c374d78afd5cba429
young-zy <youngzhaozhou@gmail.com>
2020-06-23 22:45:44 +0800

    change SQL connection pool config

--- src/main/kotlin/com/young_zy/forum/config/SQLConfig.kt
+++ src/main/kotlin/com/young_zy/forum/config/SQLConfig.kt
@@ -4,8 +4,6 @@ import com.google.gson.Gson
 import com.young_zy.forum.config.properties.SQLProperties
 import com.young_zy.forum.model.user.UserAuthReadConverter
 import com.young_zy.forum.model.user.UserAuthWriteConverter
-import io.r2dbc.pool.ConnectionPool
-import io.r2dbc.pool.ConnectionPoolConfiguration
 import io.r2dbc.pool.PoolingConnectionFactoryProvider.MAX_SIZE
 import io.r2dbc.spi.ConnectionFactories
 import io.r2dbc.spi.ConnectionFactory
@@ -20,33 +18,23 @@ import org.springframework.data.r2dbc.core.DatabaseClient
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories
 import org.springframework.transaction.annotation.EnableTransactionManagement
 import org.springframework.transaction.reactive.TransactionalOperator
-import java.time.Duration
 
 @Configuration
 @EnableR2dbcRepositories
 @EnableTransactionManagement
 class SQLConfig(@Autowired val sqlProperties: SQLProperties) : AbstractR2dbcConfiguration() {
 
-    private val connectionFactoryBuild = ConnectionFactories.get(
-            builder()
-                    .option(DRIVER, "pool")
-                    .option(PROTOCOL, "mysql")
-                    .option(HOST, sqlProperties.host)
-                    .option(USER, sqlProperties.username)
-                    .option(PASSWORD, sqlProperties.password)
-                    .option(MAX_SIZE, sqlProperties.maxConnection)
-                    .option(DATABASE, sqlProperties.database)
-                    .build()
-    )
-
     @Bean
     override fun connectionFactory(): ConnectionFactory {
-        return ConnectionPool(
-                ConnectionPoolConfiguration.builder(connectionFactoryBuild)
-                        .maxIdleTime(Duration.ofMinutes(30))
-                        .initialSize(5)
-                        .maxSize(30)
-                        .maxCreateConnectionTime(Duration.ofSeconds(1))
+        return ConnectionFactories.get(
+                builder()
+                        .option(DRIVER, "pool")
+                        .option(PROTOCOL, "mysql")
+                        .option(HOST, sqlProperties.host)
+                        .option(USER, sqlProperties.username)
+                        .option(PASSWORD, sqlProperties.password)
+                        .option(MAX_SIZE, sqlProperties.maxConnection)
+                        .option(DATABASE, sqlProperties.database)
                         .build()
         )
     }

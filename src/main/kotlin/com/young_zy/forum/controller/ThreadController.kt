bf0d5e9b20e7384b0b3b6394a30009ebac27ac0b
young-zy <youngzhaozhou@gmail.com>
2020-07-17 10:17:42 +0800

    remove exception handling in thread controller

--- src/main/kotlin/com/young_zy/forum/controller/ThreadController.kt
+++ src/main/kotlin/com/young_zy/forum/controller/ThreadController.kt
@@ -1,17 +1,11 @@
 package com.young_zy.forum.controller
 
-import com.young_zy.forum.config.stackTraceString
 import com.young_zy.forum.controller.request.PostThreadRequest
 import com.young_zy.forum.controller.request.ReplyRequest
 import com.young_zy.forum.controller.response.*
 import com.young_zy.forum.service.HitRateService
 import com.young_zy.forum.service.RateLimitService
 import com.young_zy.forum.service.ThreadService
-import com.young_zy.forum.service.exception.AuthException
-import com.young_zy.forum.service.exception.NotFoundException
-import com.young_zy.forum.service.exception.RateLimitExceededException
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
@@ -29,8 +23,6 @@ class ThreadController {
     @Autowired
     lateinit var hitRateService: HitRateService
 
-    val logger: Logger = LoggerFactory.getLogger(this.javaClass)
-
     @PostMapping("/thread/{threadId}/reply")
     suspend fun postReply(@RequestHeader headers: Map<String, String>,
                           @PathVariable threadId: Long,
@@ -38,27 +30,12 @@ class ThreadController {
         var responseBody: Response? = null
         var status = HttpStatus.OK
         val responseHeaders = HttpHeaders()
-        try {
-            rateLimitService.buildHeader(headers, responseHeaders)
-            threadService.postReply(headers["token"] ?: "", threadId, replyRequest.replyContent)
-        } catch (e: RateLimitExceededException) {
-            status = HttpStatus.TOO_MANY_REQUESTS
-            responseBody = Response(false, e.message)
-        } catch (e: NotFoundException) {
-            status = HttpStatus.NOT_FOUND
-            responseBody = Response(false, e.message)
-        } catch (e: AuthException) {
-            status = HttpStatus.UNAUTHORIZED
-        } catch (e: Exception) {
-            status = HttpStatus.INTERNAL_SERVER_ERROR
-            logger.error(e.stackTraceString)
-            responseBody = Response(false, e.message ?: "")
-        } finally {
-            return ResponseEntity
-                    .status(status)
-                    .headers(responseHeaders)
-                    .body(responseBody)
-        }
+        rateLimitService.buildHeader(headers, responseHeaders)
+        threadService.postReply(headers["token"] ?: "", threadId, replyRequest.replyContent)
+        return ResponseEntity
+                .status(status)
+                .headers(responseHeaders)
+                .body(responseBody)
     }
 
     @PostMapping("/thread")
@@ -69,30 +46,18 @@ class ThreadController {
         var responseBody: Response? = null
         var status = HttpStatus.OK
         val responseHeaders = HttpHeaders()
-        try {
-            rateLimitService.buildHeader(headers, responseHeaders)
-            threadService.postThread(
-                    headers["token"],
-                    requestBody.sectionId,
-                    requestBody.title,
-                    requestBody.content,
-                    requestBody.isQuestion
-            )
-        } catch (e: RateLimitExceededException) {
-            status = HttpStatus.TOO_MANY_REQUESTS
-            responseBody = Response(false, e.message)
-        } catch (e: AuthException) {
-            status = HttpStatus.UNAUTHORIZED
-        } catch (e: Exception) {
-            status = HttpStatus.INTERNAL_SERVER_ERROR
-            logger.error(e.stackTraceString)
-            responseBody = Response(false, e.message ?: "")
-        } finally {
-            return ResponseEntity
-                    .status(status)
-                    .headers(responseHeaders)
-                    .body(responseBody)
-        }
+        rateLimitService.buildHeader(headers, responseHeaders)
+        threadService.postThread(
+                headers["token"],
+                requestBody.sectionId,
+                requestBody.title,
+                requestBody.content,
+                requestBody.isQuestion
+        )
+        return ResponseEntity
+                .status(status)
+                .headers(responseHeaders)
+                .body(responseBody)
     }
 
     @GetMapping("/thread/{threadId}")
@@ -100,61 +65,32 @@ class ThreadController {
             @RequestHeader headers: Map<String, String>,
             @PathVariable("threadId") threadId: Long,
             @RequestParam("page") page: Int?,
-            @RequestParam("size") size: Int?
+            @RequestParam("size") size: Int?,
+            @RequestParam("orderBy") orderBy: String?
     ): ResponseEntity<*> {
         var responseBody: Response? = null
         var status = HttpStatus.OK
         val responseHeaders = HttpHeaders()
-        try {
-            rateLimitService.buildHeader(headers, responseHeaders)
-            val thread = threadService.getThread(headers["token"] ?: "", threadId, page ?: 1, size ?: 10)
-            responseBody = ThreadResponse(thread)
-        } catch (e: RateLimitExceededException) {
-            status = HttpStatus.TOO_MANY_REQUESTS
-            responseBody = Response(false, e.message)
-        } catch (e: NotFoundException) {
-            status = HttpStatus.NOT_FOUND
-            responseBody = Response(false, e.message)
-        } catch (e: AuthException) {
-            status = HttpStatus.UNAUTHORIZED
-        } catch (e: Exception) {
-            status = HttpStatus.INTERNAL_SERVER_ERROR
-            logger.error(e.stackTraceString)
-            responseBody = Response(false, e.message ?: "")
-        } finally {
-            return ResponseEntity
-                    .status(status)
-                    .headers(responseHeaders)
-                    .body(responseBody)
-        }
+        rateLimitService.buildHeader(headers, responseHeaders)
+        val thread = threadService.getThread(headers["token"] ?: "", threadId, page ?: 1, size ?: 10, orderBy
+                ?: "priority")
+        responseBody = ThreadResponse(thread)
+        return ResponseEntity
+                .status(status)
+                .headers(responseHeaders)
+                .body(responseBody)
     }
 
     @PutMapping("/reply/{replyId}/vote")
     suspend fun updateVote(@PathVariable replyId: Long, @RequestHeader headers: Map<String, String>, @RequestParam state: Long): ResponseEntity<Response> {
-        var responseBody: Response? = null
-        var status = HttpStatus.OK
+        val responseBody: Response? = null
         val responseHeaders = HttpHeaders()
-        try {
-            rateLimitService.buildHeader(headers, responseHeaders)
-            threadService.vote(headers["token"] ?: "", replyId, state)
-        } catch (e: RateLimitExceededException) {
-            status = HttpStatus.TOO_MANY_REQUESTS
-            responseBody = Response(false, e.message)
-        } catch (e: NotFoundException) {
-            status = HttpStatus.NOT_FOUND
-            responseBody = Response(false, e.message)
-        } catch (e: AuthException) {
-            status = HttpStatus.UNAUTHORIZED
-        } catch (e: Exception) {
-            status = HttpStatus.INTERNAL_SERVER_ERROR
-            logger.error(e.stackTraceString)
-            responseBody = Response(false, e.message ?: "")
-        } finally {
-            return ResponseEntity
-                    .status(status)
-                    .headers(responseHeaders)
-                    .body(responseBody)
-        }
+        rateLimitService.buildHeader(headers, responseHeaders)
+        threadService.vote(headers["token"] ?: "", replyId, state)
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .headers(responseHeaders)
+                .body(responseBody)
     }
 
     @PutMapping("/reply/{replyId}")
@@ -164,27 +100,12 @@ class ThreadController {
         var responseBody: Response? = null
         var status = HttpStatus.OK
         val responseHeaders = HttpHeaders()
-        try {
-            rateLimitService.buildHeader(headers, responseHeaders)
-            threadService.updateReply(headers["token"] ?: "", replyId, requestBody.replyContent)
-        } catch (e: RateLimitExceededException) {
-            status = HttpStatus.TOO_MANY_REQUESTS
-            responseBody = Response(false, e.message)
-        } catch (e: NotFoundException) {
-            status = HttpStatus.NOT_FOUND
-            responseBody = Response(false, e.message)
-        } catch (e: AuthException) {
-            status = HttpStatus.UNAUTHORIZED
-        } catch (e: Exception) {
-            status = HttpStatus.INTERNAL_SERVER_ERROR
-            logger.error(e.stackTraceString)
-            responseBody = Response(false, e.message ?: "")
-        } finally {
-            return ResponseEntity
-                    .status(status)
-                    .headers(responseHeaders)
-                    .body(responseBody)
-        }
+        rateLimitService.buildHeader(headers, responseHeaders)
+        threadService.updateReply(headers["token"] ?: "", replyId, requestBody.replyContent)
+        return ResponseEntity
+                .status(status)
+                .headers(responseHeaders)
+                .body(responseBody)
     }
 
     @DeleteMapping("/reply/{replyId}")
@@ -192,27 +113,12 @@ class ThreadController {
         var responseBody: Response? = null
         var status = HttpStatus.OK
         val responseHeaders = HttpHeaders()
-        try {
-            rateLimitService.buildHeader(headers, responseHeaders)
-            threadService.deleteReply(headers["token"] ?: "", replyId)
-        } catch (e: RateLimitExceededException) {
-            status = HttpStatus.TOO_MANY_REQUESTS
-            responseBody = Response(false, e.message)
-        } catch (e: NotFoundException) {
-            status = HttpStatus.NOT_FOUND
-            responseBody = Response(false, e.message)
-        } catch (e: AuthException) {
-            status = HttpStatus.UNAUTHORIZED
-        } catch (e: Exception) {
-            status = HttpStatus.INTERNAL_SERVER_ERROR
-            logger.error(e.stackTraceString)
-            responseBody = Response(false, e.message ?: "")
-        } finally {
-            return ResponseEntity
-                    .status(status)
-                    .headers(responseHeaders)
-                    .body(responseBody)
-        }
+        rateLimitService.buildHeader(headers, responseHeaders)
+        threadService.deleteReply(headers["token"] ?: "", replyId)
+        return ResponseEntity
+                .status(status)
+                .headers(responseHeaders)
+                .body(responseBody)
     }
 
     @DeleteMapping("/thread/{threadId}")
@@ -220,27 +126,12 @@ class ThreadController {
         var responseBody: Response? = null
         var status = HttpStatus.OK
         val responseHeaders = HttpHeaders()
-        try {
-            rateLimitService.buildHeader(headers, responseHeaders)
-            threadService.deleteThread(headers["token"] ?: "", threadId)
-        } catch (e: RateLimitExceededException) {
-            status = HttpStatus.TOO_MANY_REQUESTS
-            responseBody = Response(false, e.message)
-        } catch (e: NotFoundException) {
-            status = HttpStatus.NOT_FOUND
-            responseBody = Response(false, e.message)
-        } catch (e: AuthException) {
-            status = HttpStatus.UNAUTHORIZED
-        } catch (e: Exception) {
-            status = HttpStatus.INTERNAL_SERVER_ERROR
-            logger.error(e.stackTraceString)
-            responseBody = Response(false, e.message ?: "")
-        } finally {
-            return ResponseEntity
-                    .status(status)
-                    .headers(responseHeaders)
-                    .body(responseBody)
-        }
+        rateLimitService.buildHeader(headers, responseHeaders)
+        threadService.deleteThread(headers["token"] ?: "", threadId)
+        return ResponseEntity
+                .status(status)
+                .headers(responseHeaders)
+                .body(responseBody)
     }
 
     @GetMapping("/reply/{replyId}")
@@ -248,28 +139,12 @@ class ThreadController {
         var responseBody: Response? = null
         var status = HttpStatus.OK
         val responseHeaders = HttpHeaders()
-        try {
-            rateLimitService.buildHeader(headers, responseHeaders)
-            responseBody = ReplyResponse(threadService.getReply(headers["token"] ?: "", replyId))
-        } catch (e: RateLimitExceededException) {
-            status = HttpStatus.TOO_MANY_REQUESTS
-            responseBody = Response(false, e.message)
-        } catch (e: NotFoundException) {
-            status = HttpStatus.NOT_FOUND
-            responseBody = Response(false, e.message)
-        } catch (e: AuthException) {
-            status = HttpStatus.UNAUTHORIZED
-            responseBody = Response(false, e.message)
-        } catch (e: Exception) {
-            status = HttpStatus.INTERNAL_SERVER_ERROR
-            logger.error(e.stackTraceString)
-            responseBody = Response(false, e.message ?: "")
-        } finally {
-            return ResponseEntity
-                    .status(status)
-                    .headers(responseHeaders)
-                    .body(responseBody)
-        }
+        rateLimitService.buildHeader(headers, responseHeaders)
+        responseBody = ReplyResponse(threadService.getReply(headers["token"] ?: "", replyId))
+        return ResponseEntity
+                .status(status)
+                .headers(responseHeaders)
+                .body(responseBody)
     }
 
     @GetMapping("/search")
@@ -278,27 +153,12 @@ class ThreadController {
         var responseBody: Response? = null
         var status = HttpStatus.OK
         val responseHeaders = HttpHeaders()
-        try {
-            rateLimitService.buildHeader(headers, responseHeaders)
-            responseBody = SearchResponse(threadService.search(headers["token"] ?: "", keyWord, page ?: 1, size ?: 10))
-        } catch (e: RateLimitExceededException) {
-            status = HttpStatus.TOO_MANY_REQUESTS
-            responseBody = Response(false, e.message)
-        } catch (e: NotFoundException) {
-            status = HttpStatus.NOT_FOUND
-            responseBody = Response(false, e.message)
-        } catch (e: AuthException) {
-            status = HttpStatus.UNAUTHORIZED
-        } catch (e: Exception) {
-            status = HttpStatus.INTERNAL_SERVER_ERROR
-            logger.error(e.stackTraceString)
-            responseBody = Response(false, e.message ?: "")
-        } finally {
-            return ResponseEntity
-                    .status(status)
-                    .headers(responseHeaders)
-                    .body(responseBody)
-        }
+        rateLimitService.buildHeader(headers, responseHeaders)
+        responseBody = SearchResponse(threadService.search(headers["token"] ?: "", keyWord, page ?: 1, size ?: 10))
+        return ResponseEntity
+                .status(status)
+                .headers(responseHeaders)
+                .body(responseBody)
     }
 
     @GetMapping("/thread/hot")
@@ -306,26 +166,11 @@ class ThreadController {
         var responseBody: Response? = null
         var status = HttpStatus.OK
         val responseHeaders = HttpHeaders()
-        try {
-            rateLimitService.buildHeader(headers, responseHeaders)
-            responseBody = HitRateResponse(hitRateService.getOrder(count ?: 20))
-        } catch (e: RateLimitExceededException) {
-            status = HttpStatus.TOO_MANY_REQUESTS
-            responseBody = Response(false, e.message)
-        } catch (e: NotFoundException) {
-            status = HttpStatus.NOT_FOUND
-            responseBody = Response(false, e.message)
-        } catch (e: AuthException) {
-            status = HttpStatus.UNAUTHORIZED
-        } catch (e: Exception) {
-            status = HttpStatus.INTERNAL_SERVER_ERROR
-            logger.error(e.stackTraceString)
-            responseBody = Response(false, e.message ?: "")
-        } finally {
-            return ResponseEntity
-                    .status(status)
-                    .headers(responseHeaders)
-                    .body(responseBody)
-        }
+        rateLimitService.buildHeader(headers, responseHeaders)
+        responseBody = HitRateResponse(hitRateService.getOrder(count ?: 20))
+        return ResponseEntity
+                .status(status)
+                .headers(responseHeaders)
+                .body(responseBody)
     }
 }
